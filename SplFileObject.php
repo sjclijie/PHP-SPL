1b2090b0c62a2f29c911513ab9efdff87beb6eb4
jaylee <sjclijie@vip.qq.com>
2016-03-20 23:27:36 +0800

    update

--- SplFileObject.php
+++ SplFileObject.php
@@ -6,3 +6,33 @@
  * Time: 22:57
  */
 
+/**
+ *
+ * 用来对文本文件进行遍历
+ *
+ */
+
+$fileName = './LimitIterator.php';
+
+try {
+
+    foreach (new SplFileObject( $fileName, 'r' ) as $line ){
+        echo $line . "<br />";
+    }
+} catch ( Exception $e ){
+
+    var_dump( $e->getMessage());
+}
+
+echo "<hr />";
+
+//返回文件前三行
+try {
+
+    foreach ( new LimitIterator( new SplFileObject( $fileName, 'r' ), 0, 3) as $line ) {
+        echo $line . "<br />";
+    }
+
+}catch ( Exception $e) {
+
+}
